de65a49e4c8b5b935c3883fd74e44330a4637128
Joseph Pierre GOMIS <gomisjoepierre@gmail.com>
2021-07-08 20:12:16 +0000

    Make use of webpack encore & add lang attribute to html tag

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Pin;
+use PhpParser\Node\Expr\Cast\String_;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
